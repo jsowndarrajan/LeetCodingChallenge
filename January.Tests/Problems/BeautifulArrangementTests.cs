240cde2651c30861b0423feedef0a9c13a1c5417
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-03 23:36:15 +0800

    Day 3: Beautiful Arrangement

---
+++ January.Tests/Problems/BeautifulArrangementTests.cs
@@ -0,0 +1,37 @@
+﻿using January.Problems;
+using NUnit.Framework;
+
+namespace January.Tests.Problems
+{
+    public class BeautifulArrangementTests
+    {
+        private BeautifulArrangement _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new BeautifulArrangement();
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 8)]
+        [TestCase(5, 10)]
+        [TestCase(6, 36)]
+        [TestCase(7, 41)]
+        [TestCase(8, 132)]
+        [TestCase(9, 250)]
+        [TestCase(10, 700)]
+        [TestCase(11, 750)]
+        [TestCase(12, 4010)]
+        [TestCase(13, 4237)]
+        [TestCase(14, 10680)]
+        [TestCase(15, 24679)]
+        public void CanFormArray(int n, int expected)
+        {
+            var actual = _sut.CountArrangement(n);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ January/Problems/BeautifulArrangement.cs
@@ -0,0 +1,61 @@
+﻿using Shared.Helpers;
+using Shared.Interfaces;
+
+namespace January.Problems
+{
+    public class BeautifulArrangement : IProblem
+    {
+        public string Title => "Beautiful Arrangement";
+
+        public string Url =>
+            "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3591/";
+
+        public string CountArrangement(string input)
+        {
+            if (int.TryParse(input, out var value) && value >= 1 && value <= 15)
+            {
+                return CountArrangement(value).ToString();
+            }
+            return Messages.InvalidInput;
+        }
+
+        public int CountArrangement(int n)
+        {
+            var array = new int[n+1];
+            for (var i = 1; i <= n; i++)
+            {
+                array[i] = i;
+            }
+
+            var result = 0;
+            CountArrangement(array, n, ref result);
+            return result;
+        }
+
+        private void CountArrangement(int[] array, int n, ref int result)
+        {
+            if (n == 0)
+            {
+                result++;
+                return;
+            }
+
+            for (var i = n; i >0; i--)
+            {
+                Swap(array, i, n);
+                if (array[n] % n == 0 | n % array[n] == 0)
+                {
+                    CountArrangement(array, n - 1, ref result);
+                }
+                Swap(array, n, i);
+            }
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
