55b8e9e3344a99c8ccfa460c719e8421efdeec60
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-06 00:24:58 +0800

    Day 5: Remove duplicates from list

---
+++ January.Tests/Problems/RemoveDuplicatesFromListTests.cs
@@ -0,0 +1,58 @@
+﻿using January.Problems;
+using NUnit.Framework;
+using Shared.Models;
+
+namespace January.Tests.Problems
+{
+    public class RemoveDuplicatesFromListTests
+    {
+        private RemoveDuplicatesFromList _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new RemoveDuplicatesFromList();
+        }
+
+        [Test, TestCaseSource(nameof(_testCaseSource))]
+        public void DeleteDuplicatesTest(ListNode input, ListNode expected)
+        {
+            var actual = _sut.DeleteDuplicates(input);
+            Assert.AreEqual(expected?.ToString(), actual?.ToString());
+        }
+
+        private static readonly object[] _testCaseSource =
+        {
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(2))),
+                new ListNode(1),
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3)))),
+                new ListNode(1, new ListNode(3)),
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3))))),
+                new ListNode(2, new ListNode(3)),
+            },
+            new object[]
+            {
+                new ListNode(1),
+                new ListNode(1)
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(1)),
+                null
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5))))))),
+                new ListNode(1, new ListNode(2, new ListNode(5))),
+            },
+        };
+    }
+}
---
+++ January/Problems/RemoveDuplicatesFromList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Shared.Helpers;
+using Shared.Interfaces;
+using Shared.Models;
+
+namespace January.Problems
+{
+    public class RemoveDuplicatesFromList : IProblem
+    {
+        public string Title => "Remove duplicates from list";
+        public string Url => "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3593/";
+
+        public string DeleteDuplicates(string inputArray)
+        {
+            try
+            {
+                var listNode = ListHelper.ConvertStringArrayToListNode(inputArray);
+                var result = DeleteDuplicates(listNode);
+                return $"[{result}]";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var currentNode = head;
+            ListNode duplicateNode = null;
+            ListNode startNode = null;
+            ListNode tailNode = null;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == currentNode.Next?.Value || 
+                    (duplicateNode != null && duplicateNode.Value == currentNode.Value))
+                {
+                    duplicateNode = currentNode;
+                    currentNode = currentNode.Next;
+                    if (tailNode != null)
+                    {
+                        tailNode.Next = currentNode;
+                    }
+                    continue;
+                }
+
+                if (startNode == null)
+                {
+                    startNode = currentNode;
+                    tailNode = startNode;
+                }
+                else
+                {
+                    tailNode.Next = currentNode;
+                    tailNode = tailNode.Next;
+                }
+
+                currentNode = currentNode.Next;
+            }
+            return startNode;
+        }
+    }
+}
