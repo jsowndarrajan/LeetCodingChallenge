513ff70efab603b67a093b6364415967b2cd593e
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-06 19:43:18 +0800

    Day 6: Kth Missing Positive Number

---
+++ January.Tests/Problems/KthMissingPositiveNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using January.Problems;
+using NUnit.Framework;
+
+namespace January.Tests.Problems
+{
+    public class KthMissingPositiveNumberTests
+    {
+        private KthMissingPositiveNumber _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new KthMissingPositiveNumber();
+        }
+
+        [TestCase(new[] { 2, 3, 4, 7, 11 }, 5, 9)]
+        [TestCase(new[] { 1, 2, 3, 4 }, 2, 6)]
+        [TestCase(new[] { 5, 6, 7 }, 2, 2)]
+        [TestCase(new[] { 5, 6, 7 }, 5, 8)]
+        public void FindKthPositive(int[] input, int k, int expected)
+        {
+            var actual = _sut.FindKthPositive(input, k);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ January/Problems/KthMissingPositiveNumber.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Shared.Helpers;
+using Shared.Interfaces;
+
+namespace January.Problems
+{
+    public class KthMissingPositiveNumber : IProblem
+    {
+        public string Title => "Kth Missing Positive Number";
+        public string Url => "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3594/";
+
+        public string FindKthPositive(string inputArray, string k)
+        {
+            try
+            {
+                if (!int.TryParse(k, out var targetNumber)) return Messages.InvalidInput;
+                var input = DataConverter.ConvertStringToIntArray(inputArray);
+                var result = FindKthPositive(input, targetNumber);
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public int FindKthPositive(int[] arr, int k)
+        {
+            var result = 0;
+            for (int i = 0, j = 1; i < arr.Length && k >= 1; j++)
+            {
+                if (arr[i] != j)
+                {
+                    result = j;
+                    k--;
+                }
+                else
+                {
+                    result = arr[i];
+                    i++;
+                }
+            }
+            return (result + k);
+        }
+    }
+}
