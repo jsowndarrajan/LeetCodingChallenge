6713c7a70a63c6e13fb0da837ebb74f725873be7
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-12 00:06:22 +0800

    Update readme

--- January/Problems/MergeSortedArray.cs
+++ January/Problems/MergeSortedArray.cs
@@ -1,11 +1,32 @@
-﻿using Shared.Interfaces;
+﻿using Shared.Helpers;
+using Shared.Interfaces;
+using System;
 
-namespace January.Tests.Problems
+namespace January.Problems
 {
     public class MergeSortedArray : IProblem
     {
         public string Title => "Merge Sorted Array";
         public string Url => "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3600/";
+
+
+        public string Merge(string nums1, string m, string nums2, string n)
+        {
+            try
+            {
+                var arr1 = DataConverter.ConvertStringToIntArray(nums1);
+                var arr2 = DataConverter.ConvertStringToIntArray(nums2);
+                var p = Convert.ToInt32(m);
+                var q = Convert.ToInt32(n);
+                Merge(arr1, p, arr2, q);
+
+                return $"[{string.Join(',', arr1)}]";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
 
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
--- README.md
+++ README.md
@@ -26,3 +26,5 @@ You can also execute the application in command prompt by using the below comman
 7. [Longest Substring Without Repeating Characters](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3595/)
 8. [Check If Two String Arrays are Equivalent](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3597/)
 9. [Word Ladder](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3598/)
+10. [Create Sorted Array through Instructions](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3599/)
+11. [Merge Sorted Array](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3600/)
