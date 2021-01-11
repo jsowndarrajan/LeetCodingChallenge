f7771e209ce8e89ff9bb464510483ea80af6d35b
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-11 23:09:42 +0800

    Day 11: Merge Sorted Array

---
+++ January.Tests/Problems/MergeSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using Shared.Interfaces;
+
+namespace January.Tests.Problems
+{
+    public class MergeSortedArray : IProblem
+    {
+        public string Title => "Merge Sorted Array";
+        public string Url => "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3600/";
+
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var length = m + n;
+            for (var i = n-1; i >= 0 && length > 0;)
+            {
+                if (m == 0)
+                {
+                    nums1[--length] = nums2[i];
+                    i--;
+                    continue;
+                }
+
+                if (nums2[i] > nums1[m - 1])
+                {
+                    nums1[--length] = nums2[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[--length] = nums1[--m];
+                }
+            }
+        }
+    }
+}
---
+++ January.Tests/Problems/MergeSortedArrayTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace January.Tests.Problems
+{
+    public class MergeSortedArrayTests
+    {
+        private MergeSortedArray _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new MergeSortedArray();
+        }
+
+        [TestCase(new[] { 0 }, 0, new[] { 2 }, 1, new[] { 2 })]
+        [TestCase(new[] { 1, 2, 3, 0, 0, 0 }, 3, new[] { 2, 5, 6 }, 3, new[] { 1, 2, 2, 3, 5, 6 })]
+        [TestCase(new[] { 4, 5, 6, 0, 0, 0 }, 3, new[] { 1, 2, 3 }, 3, new[] { 1, 2, 3, 4, 5, 6 })]
+        public void Merge(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+            _sut.Merge(nums1, m, nums2, n);
+            Assert.AreEqual(expected, nums1);
+        }
+    }
+}
