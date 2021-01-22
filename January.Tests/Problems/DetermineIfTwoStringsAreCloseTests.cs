f1af550b2128a249525ca71724839bffa5ac44c0
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-01-23 00:27:49 +0800

    Day 22: Determine if Two Strings Are Close

---
+++ January.Tests/Problems/DetermineIfTwoStringsAreCloseTests.cs
@@ -0,0 +1,19 @@
+﻿using January.Problems;
+using NUnit.Framework;
+
+namespace January.Tests.Problems
+{
+    public class DetermineIfTwoStringsAreCloseTests
+    {
+        [TestCase("abc","bca", true)]
+        [TestCase("a","aa", false)]
+        [TestCase("cabbba", "abbccc", true)]
+        [TestCase("cabbba", "aabbss", false)]
+        public void CloseStrings(string word1, string word2, bool expected)
+        {
+            var sut = new DetermineIfTwoStringsAreClose();
+            var actual = sut.CloseStrings(word1, word2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ January/Problems/DetermineIfTwoStringsAreClose.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Shared.Interfaces;
+
+namespace January.Problems
+{
+    public class DetermineIfTwoStringsAreClose : IProblem
+    {
+        public string Title => "Determine if Two Strings Are Close";
+        public string Url => "https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3613/";
+
+        public bool CloseStrings(string word1, string word2)
+        {
+            if (word1.Length != word2.Length) return false;
+
+            var charactersCountInWord1 = new int[26];
+            var charactersCountInWord2 = new int[26];
+            var availableCharactersInWord1 = new bool[26];
+            var availableCharactersInWord2 = new bool[26];
+
+            for (var i = 0; i < word1.Length; i++)
+            {
+                var index1 = word1[i] - 'a';
+                var index2 = word2[i] - 'a';
+                charactersCountInWord1[index1] += 1;
+                charactersCountInWord2[index2] += 1;
+                availableCharactersInWord1[index1] = true;
+                availableCharactersInWord2[index2] = true;
+            }
+
+            Array.Sort(charactersCountInWord1);
+            Array.Sort(charactersCountInWord2);
+
+            for (var i = 0; i < 26; i++)
+            {
+                if (!(charactersCountInWord1[i] == charactersCountInWord2[i]
+                    &&
+                    availableCharactersInWord1[i] == availableCharactersInWord2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
