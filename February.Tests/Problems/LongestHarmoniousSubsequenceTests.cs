47d822d32bba3c6c5788ba1c71161b67da878ac0
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-02-04 22:57:39 +0800

    Day 4: Longest Harmonious Subsequence

---
+++ February.Tests/Problems/LongestHarmoniousSubsequenceTests.cs
@@ -0,0 +1,18 @@
+﻿using February.Problems;
+using NUnit.Framework;
+
+namespace February.Tests.Problems
+{
+    public class LongestHarmoniousSubsequenceTests
+    {
+        [TestCase(new[] { 1, 3, 2, 2, 5, 2, 3, 7 }, 5)]
+        [TestCase(new[] { 1, 2, 3, 4 }, 2)]
+        [TestCase(new[] { 1, 1, 1, 1 }, 0)]
+        public void FindLHS(int[] input, int expected)
+        {
+            var sut = new LongestHarmoniousSubsequence();
+            var actual = sut.FindLHS(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ February/Problems/LongestHarmoniousSubsequence.cs
@@ -0,0 +1,53 @@
+﻿using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using Shared.Helpers;
+
+namespace February.Problems
+{
+    public class LongestHarmoniousSubsequence : IProblem
+    {
+        public string Title => "Longest Harmonious Subsequence";
+        public string Url => "https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3628/";
+
+        public string FindLHS(string input)
+        {
+            try
+            {
+                var nums = DataConverter.ConvertStringToIntArray(input);
+                var result = FindLHS(nums);
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public int FindLHS(int[] nums)
+        {
+            var map = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    map[nums[i]] = map[nums[i]] + 1;
+                }
+                else
+                {
+                    map.Add(nums[i], 1);
+                }
+            }
+
+            var output = 0;
+            foreach (var kp in map)
+            {
+                if (map.ContainsKey(kp.Key + 1))
+                {
+                    output = Math.Max(output, kp.Value + map[kp.Key + 1]);
+                }
+            }
+            return output;
+        }
+    }
+}
