2fee756ffd76eea0b500f81abaa81f85af01b5e8
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-02-07 00:33:11 +0800

    Day 5: Simplify Path

---
+++ February.Tests/Problems/SimplifyPathTests.cs
@@ -0,0 +1,41 @@
+﻿using February.Problems;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace February.Tests.Problems
+{
+    public class SimplifyPathTests
+    {
+        [TestCase("/home/", "/home")]
+        [TestCase("/../", "/")]
+        [TestCase("/home//foo/", "/home/foo")]
+        [TestCase("/a/./b/../../c/", "/c")]
+        public void GenerateCanonicalPath(string input, string expected)
+        {
+            var sut = new SimplifyPath();
+            var actual = sut.GenerateCanonicalPath(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ConvertToCanonicalPathFromFilledStack()
+        {
+            var commands = new Stack<string>();
+            commands.Push("a");
+            commands.Push("c");
+            commands.Push("d");
+            var sut = new SimplifyPath();
+            var actual = sut.ConvertToCanonicalPath(commands);
+            Assert.AreEqual("/a/c/d", actual);
+        }
+
+        [Test]
+        public void ConvertToCanonicalPathFromEmptyStack()
+        {
+            var commands = new Stack<string>();
+            var sut = new SimplifyPath();
+            var actual = sut.ConvertToCanonicalPath(commands);
+            Assert.AreEqual("/", actual);
+        }
+    }
+}
---
+++ February/Problems/SimplifyPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Shared.Interfaces;
+
+namespace February.Problems
+{
+    public class SimplifyPath : IProblem
+    {
+        public string Title => "Simplify Path";
+        public string Url => "https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/584/week-1-february-1st-february-7th/3629/";
+
+        public string GenerateCanonicalPath(string input)
+        {
+            var commands = new Stack<string>();
+            var paths = input.Split('/');
+            foreach (var path in paths)
+            {
+                if(path == string.Empty) continue;
+                switch (path)
+                {
+                    case "":
+                    case ".":
+                        break;
+                    case "..":
+                    {
+                        if (commands.Count > 0)
+                        {
+                            commands.Pop();
+                        }
+                        break;
+                    }
+                    default:
+                        commands.Push(path);
+                        break;
+                }
+            }
+            return ConvertToCanonicalPath(commands);
+        }
+
+        public string ConvertToCanonicalPath(Stack<string> stack)
+        {
+            if (stack.Count == 0) return "/";
+
+            var result = string.Empty;
+            while (stack.Count > 0)
+            {
+                var dirPath = stack.Pop();
+                result = "/" + dirPath + result;
+            }
+            return result;
+        }
+    }
+}
