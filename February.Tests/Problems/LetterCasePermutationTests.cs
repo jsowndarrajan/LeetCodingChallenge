afd08544f39cb2674d521472dbc3f7517c9d77f7
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-02-17 01:02:28 +0800

    Day 16: Letter Case Permutation

---
+++ February.Tests/Problems/LetterCasePermutationTests.cs
@@ -0,0 +1,20 @@
+﻿using February.Problems;
+using NUnit.Framework;
+using System.Linq;
+
+namespace February.Tests.Problems
+{
+    public class LetterCasePermutationTests
+    {
+        [TestCase("a1b2", new[] { "a1b2", "a1B2", "A1b2", "A1B2" })]
+        [TestCase("3z4", new[] { "3z4", "3Z4" })]
+        [TestCase("12345", new[] { "12345" })]
+        [TestCase("0", new[] { "0" })]
+        public void Permute(string input, string[] expected)
+        {
+            var sut = new LetterCasePermutation();
+            var actual = sut.ComputeLetterCasePermutation(input);
+            Assert.AreEqual(0, expected.Except(actual).Count());
+        }
+    }
+}
---
+++ February/Problems/LetterCasePermutation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Shared.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace February.Problems
+{
+    public class LetterCasePermutation : IProblem
+    {
+        private const int CharacterOffset = 32;
+        public string Title => "Letter Case Permutation";
+        public string Url => "https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/586/week-3-february-15th-february-21st/3642/";
+
+        public string Execute(string input)
+        {
+            try
+            {
+                var result = ComputeLetterCasePermutation(input);
+                return JsonConvert.SerializeObject(result);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public IList<string> ComputeLetterCasePermutation(string input)
+        {
+            var output = new List<string>();
+            Permute(input.ToCharArray(), 0, output);
+            return output.ToList();
+        }
+
+        private static void Permute(char[] input, int start, List<string> output)
+        {
+            if (start == input.Length)
+            {
+                output.Add(new string(input));
+                return;
+            }
+
+            if (char.IsNumber(input[start]))
+            {
+                Permute(input, start + 1, output);
+                return;
+            }
+
+            Permute(input, start + 1, output);
+
+            var negativeOffset = char.IsLower(input[start])? -1 : 1;
+            input[start] += (char)(CharacterOffset * negativeOffset);
+            Permute(input, start + 1, output);
+            input[start] -= (char)(CharacterOffset * negativeOffset);
+        }
+    }
+}
