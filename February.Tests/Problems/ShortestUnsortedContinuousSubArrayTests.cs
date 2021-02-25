579cdfbd64b63e28e387863021d238e9a0928b5f
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-02-26 00:03:48 +0800

    Day 25: Shortest Unsorted Continuous Subarray

---
+++ February.Tests/Problems/ShortestUnsortedContinuousSubArrayTests.cs
@@ -0,0 +1,22 @@
+﻿using February.Problems;
+using NUnit.Framework;
+
+namespace February.Tests.Problems
+{
+    public class ShortestUnsortedContinuousSubArrayTests
+    {
+        [TestCase(new[] { 2, 6, 4, 8, 10, 9, 15 }, 5)]
+        [TestCase(new[] { 1, 2, 3, 4 }, 0)]
+        [TestCase(new[] { 5, 4, 3, 2, 1 }, 5)]
+        [TestCase(new[] { 1 }, 0)]
+        [TestCase(new[] { 2, 1 }, 2)]
+        [TestCase(new[] { 1, 3, 2, 2, 2 }, 4)]
+        [TestCase(new[] { 2, 3, 3, 2, 4 }, 3)]
+        public void FindUnsortedSubArray(int[] input, int expected)
+        {
+            var sut = new ShortestUnsortedContinuousSubArray();
+            var actual = sut.FindUnsortedSubArray(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ February/Problems/ShortestUnsortedContinuousSubArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Shared.Helpers;
+using Shared.Interfaces;
+
+namespace February.Problems
+{
+    public class ShortestUnsortedContinuousSubArray : IProblem
+    {
+        public string Title => "Shortest Unsorted Continuous Subarray";
+        public string Url => "https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3652/";
+
+        public string FindUnsortedSubArray(string input)
+        {
+            try
+            {
+                var nums = DataConverter.ConvertStringToIntArray(input);
+                var result = FindUnsortedSubArray(nums);
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public int FindUnsortedSubArray(int[] nums)
+        {
+            var map = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                map[i] = nums[i];
+            }
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var index = i;
+                while (i > 0 && map[i - 1] > map[i])
+                {
+                    var temp = map[i];
+                    map[i] = map[i - 1];
+                    map[i - 1] = temp;
+                    i--;
+                }
+                i = index;
+            }
+
+            var startIndex = -1;
+            var endIndex = -1;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != map[i])
+                {
+                    if (startIndex == -1)
+                    {
+                        startIndex = i;
+                    }
+                    endIndex = i;
+                }
+            }
+
+            return startIndex == -1 ? 0 : (endIndex - startIndex) + 1;
+        }
+    }
+}
