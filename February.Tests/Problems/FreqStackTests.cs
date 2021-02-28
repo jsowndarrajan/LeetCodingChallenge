22e15fba324466bdefa8519cf649f5c6c427dbe1
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-02-28 23:19:02 +0800

    Day 29: Maximum Frequency Stack

---
+++ February.Tests/Problems/FreqStackTests.cs
@@ -0,0 +1,53 @@
+﻿using February.Problems;
+using NUnit.Framework;
+
+namespace February.Tests.Problems
+{
+    public class FreqStackTests
+    {
+        [Test]
+        public void Validate1()
+        {
+            var sut = new FreqStack();
+            sut.Push(5);
+            sut.Push(7);
+            sut.Push(5);
+            sut.Push(7);
+            sut.Push(4);
+            sut.Push(5);
+
+            Assert.AreEqual(5, sut.Pop());
+            Assert.AreEqual(7, sut.Pop());
+            Assert.AreEqual(5, sut.Pop());
+            Assert.AreEqual(4, sut.Pop());
+            Assert.AreEqual(7, sut.Pop());
+            Assert.AreEqual(5, sut.Pop());
+        }
+
+        [Test]
+        public void Validate2()
+        {
+            var sut = new FreqStack();
+            sut.Push(4);
+            sut.Push(0);
+            sut.Push(9);
+            sut.Push(3);
+            sut.Push(4);
+            sut.Push(2);
+            Assert.AreEqual(4, sut.Pop());
+            sut.Push(6);
+            Assert.AreEqual(6, sut.Pop());
+            sut.Push(1);
+            Assert.AreEqual(1, sut.Pop());
+            sut.Push(1);
+            Assert.AreEqual(1, sut.Pop());
+            sut.Push(4);
+            Assert.AreEqual(4, sut.Pop());
+            Assert.AreEqual(2, sut.Pop());
+            Assert.AreEqual(3, sut.Pop());
+            Assert.AreEqual(9, sut.Pop());
+            Assert.AreEqual(0, sut.Pop());
+            Assert.AreEqual(4, sut.Pop());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -78,3 +78,4 @@ You can also execute the application in command prompt by using the below comman
 25. [Shortest Unsorted Continuous Subarray](https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3652/)
 26. [Validate Stack Sequences](https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3653/)
 27. [Divide Two Integers](https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3654/)
+28. [Maximum Frequency Stack](https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/587/week-4-february-22nd-february-28th/3655/)
