d5cf66eb95fde3bd1cdc5e0bc8b266a003146166
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-01 19:17:11 +0800

    Day 1: Distribute Candies

---
+++ March.Tests/Problems/DistributeCandiesTests.cs
@@ -0,0 +1,19 @@
+﻿using March.Problems;
+using NUnit.Framework;
+
+namespace March.Tests.Problems
+{
+    public class DistributeCandiesTests
+    {
+        [TestCase(new[] {1, 1}, 1)]
+        [TestCase(new[] {1, 1, 2, 3}, 2)]
+        [TestCase(new[] {1, 1, 2, 3, 2, 3}, 3)]
+        [TestCase(new[] {1, 1, 1, 1}, 1)]
+        public void Calculate(int[] candyType, int expected)
+        {
+            var sut = new DistributeCandies();
+            var actual = sut.Calculate(candyType);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ March/Problems/DistributeCandies.cs
@@ -0,0 +1,34 @@
+﻿using Shared.Helpers;
+using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace March.Problems
+{
+    public class DistributeCandies : IProblem
+    {
+        public string Title => "Distribute Candies";
+        public string Url => "https://leetcode.com/explore/featured/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3657/";
+
+        public string Calculate(string candyType)
+        {
+            try
+            {
+                var input = DataConverter.ConvertStringToIntArray(candyType);
+                var result = Calculate(input);
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public int Calculate(int[] input)
+        {
+            var max = input.Length / 2;
+            var hashset = new HashSet<int>(input);
+            return Math.Min(hashset.Count, max);
+        }
+    }
+}
