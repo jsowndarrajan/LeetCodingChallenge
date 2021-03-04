b8a471b52589e3975c152bc95fbcdbdff4d6be64
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-04 23:14:24 +0800

    Day 4: Intersection of Two Linked Lists

---
+++ March.Tests/Problems/IntersectionOfTwoLinkedListsTests.cs
@@ -0,0 +1,42 @@
+﻿using March.Problems;
+using NUnit.Framework;
+using Shared.Models;
+
+namespace March.Tests.Problems
+{
+    public class IntersectionOfTwoLinkedListsTests
+    {
+        [Test, TestCaseSource(nameof(TestCaseSource))]
+        public void GetIntersectionNode(ListNode headA, ListNode headB, ListNode expected)
+        {
+            var sut = new IntersectionOfTwoLinkedLists();
+            var actual = sut.GetIntersectionNode(headA, headB);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static object[] TestCaseSource => new object[]
+        {
+            new object[]
+            {
+                new ListNode(4, new ListNode(1, node8)),
+                new ListNode(5, new ListNode(6, new ListNode(1, node8))),
+                node8
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(9, new ListNode(1, node2))),
+                new ListNode(3, node2),
+                node2
+            },
+            new object[]
+            {
+                new ListNode(2, new ListNode(6, new ListNode(4))),
+                new ListNode(1, new ListNode(5)),
+                null
+            }
+        };
+
+        private static readonly ListNode node8 = new ListNode(8, new ListNode(4, new ListNode(5)));
+        private static readonly ListNode node2 = new ListNode(2, new ListNode(4));
+    }
+}
---
+++ March/Problems/IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Shared.Interfaces;
+using Shared.Models;
+using System.Collections.Generic;
+using Shared.Helpers;
+
+namespace March.Problems
+{
+    public class IntersectionOfTwoLinkedLists : IProblem
+    {
+        public string Title => "Intersection of Two Linked Lists";
+        public string Url => "https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3660/";
+
+        public string GetIntersectionNode(string input1, string input2)
+        {
+            try
+            {
+                var headA = ListHelper.ConvertStringArrayToListNode(input1);
+                var headB = ListHelper.ConvertStringArrayToListNode(input2);
+                var result = GetIntersectionNode(headA, headB);
+                return result is null ? "null" : result.Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var hashset = new HashSet<ListNode>();
+            while (headA != null)
+            {
+                var node = headA;
+                hashset.Add(node);
+                headA = headA.Next;
+            }
+
+            while (headB != null)
+            {
+                var node = headB;
+                if (hashset.Contains(node))
+                {
+                    return node;
+                }
+                headB = headB.Next;
+            }
+            return null;
+        }
+    }
+}
