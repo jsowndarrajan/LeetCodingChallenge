9f55af0f4cc4dd8fe3b753a94899c0ff4681e429
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-09 00:12:52 +0800

    Day 8: Remove Palindromic Subsequence

---
+++ March.Tests/Problems/RemovePalindromicSubsequenceTests.cs
@@ -0,0 +1,24 @@
+﻿using March.Problems;
+using NUnit.Framework;
+
+namespace March.Tests.Problems
+{
+    public class RemovePalindromicSubsequenceTests
+    {
+        [TestCase("", 0)]
+        [TestCase("a", 1)]
+        [TestCase("aa", 1)]
+        [TestCase("ab", 2)]
+        [TestCase("aba", 1)]
+        [TestCase("abb", 2)]
+        [TestCase("baabb", 2)]
+        [TestCase("babab", 1)]
+        [TestCase("bbaabaaa", 2)]
+        public void RemovePalindromeSub(string input, int expected)
+        {
+            var sut = new RemovePalindromicSubsequence();
+            var actual = sut.RemovePalindromeSub(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ March/Problems/RemovePalindromicSubsequence.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Interfaces;
+
+namespace March.Problems
+{
+    public class RemovePalindromicSubsequence : IProblem
+    {
+        public string Title => "Remove Palindromic Subsequence";
+        public string Url => "https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3665/";
+
+        public int RemovePalindromeSub(string input)
+        {
+            if (input.Length <= 1) return input.Length;
+            return IsPalindrome(input) ? 1 : 2;
+        }
+
+        private bool IsPalindrome(string input)
+        {
+            var i = 0;
+            var j = input.Length - 1;
+            while (i < j)
+            {
+                if (input[i] != input[j]) return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
