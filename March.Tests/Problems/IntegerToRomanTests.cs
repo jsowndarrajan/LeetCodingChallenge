b69fedb7c016e9cba5f61fbb9f98c18674169ab5
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-11 00:17:41 +0800

    Day 10: Integer to Roman

---
+++ March.Tests/Problems/IntegerToRomanTests.cs
@@ -0,0 +1,35 @@
+﻿using March.Problems;
+using NUnit.Framework;
+
+namespace March.Tests.Problems
+{
+    public class IntegerToRomanTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(20, "XX")]
+        [TestCase(40, "XL")]
+        [TestCase(50, "L")]
+        [TestCase(70, "LXX")]
+        [TestCase(100, "C")]
+        [TestCase(200, "CC")]
+        [TestCase(400, "CD")]
+        [TestCase(500, "D")]
+        [TestCase(600, "DC")]
+        [TestCase(600, "DC")]
+        [TestCase(900, "CM")]
+        [TestCase(1000, "M")]
+        [TestCase(2000, "MM")]
+        public void IntToRoman(int num, string expected)
+        {
+            var sut = new IntegerToRoman();
+            var actual = sut.IntToRoman(num);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ March/Problems/IntegerToRoman.cs
@@ -0,0 +1,56 @@
+﻿using Shared.Helpers;
+using Shared.Interfaces;
+using System.Collections.Generic;
+
+namespace March.Problems
+{
+    public class IntegerToRoman : IProblem
+    {
+        public string Title => "Integer to Roman";
+        public string Url => "https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3667/";
+
+        private Dictionary<int, string> IntRomanMap => new Dictionary<int, string>
+        {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+
+        public string IntToRoman(string input)
+        {
+            if (int.TryParse(input, out var num))
+            {
+                return IntToRoman(num);
+            }
+            return Messages.InvalidInput;
+        }
+
+        public string IntToRoman(int num)
+        {
+            var output = string.Empty;
+            foreach (var kvp in IntRomanMap)
+            {
+                var numberOfDigits = num / kvp.Key;
+                if (numberOfDigits <= 0) continue;
+
+                num %= kvp.Key;
+                do
+                {
+                    output += kvp.Value;
+                    numberOfDigits--;
+                } while (numberOfDigits > 0);
+            }
+            return output;
+        }
+    }
+}
