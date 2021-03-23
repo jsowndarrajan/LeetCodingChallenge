9b38f8575ca36a8fb1d3e7a1e68e371161a83a73
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-24 01:27:21 +0800

    Day 23: 3Sum With Multiplicity

---
+++ March.Tests/Problems/ThreeSumMultiplicityTests.cs
@@ -0,0 +1,19 @@
+﻿using March.Problems;
+using NUnit.Framework;
+
+namespace March.Tests.Problems
+{
+    public class ThreeSumMultiplicityTests
+    {
+        [TestCase(new[] { 0, 2, 0 }, 2, 1)]
+        [TestCase(new[] { 1, 0, 1, 0, 2, 1, 2 }, 1, 3)]
+        [TestCase(new[] { 1, 1, 2, 2, 2, 2 }, 5, 12)]
+        [TestCase(new[] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 }, 8, 20)]
+        public void Calculate(int[] arr, int target, int expected)
+        {
+            var sut = new ThreeSumMultiplicity();
+            var actual = sut.Calculate(arr, target);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ March/Problems/ThreeSumMultiplicity.cs
@@ -0,0 +1,61 @@
+﻿using Shared.Interfaces;
+using System.Collections.Generic;
+
+namespace March.Problems
+{
+    public class ThreeSumMultiplicity : IProblem
+    {
+        public string Title => "3Sum With Multiplicity";
+        public string Url => "https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/591/week-4-march-22nd-march-28th/3682/";
+
+        public int Calculate(int[] arr, int target)
+        {
+            long output = 0;
+            var map = new Dictionary<int, long>();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (map.ContainsKey(arr[i]))
+                {
+                    map[arr[i]] += 1;
+                }
+                else
+                {
+                    map.Add(arr[i], 1);
+                }
+            }
+
+            foreach (var kvp1 in map)
+            {
+                foreach (var kvp2 in map)
+                {
+                    var thirdElementValue = target - kvp1.Key - kvp2.Key;
+
+                    if (map.ContainsKey(thirdElementValue))
+                    {
+                        if (kvp1.Key == kvp2.Key && kvp2.Key == thirdElementValue)
+                        {
+                            // Combinations
+                            // TC1: All are same numbers
+                            // nCr => n!/r!(n-r)!
+                            // n => total of number frequency
+                            // r => count of selected numbers from the set
+                            output += ((kvp1.Value) * (kvp1.Value - 1) * (kvp1.Value - 2)) / 6;
+                        }
+                        else if (kvp1.Key == kvp2.Key && kvp2.Key != thirdElementValue)
+                        {
+                            //TC2: First two elements is same
+                            output += ((kvp1.Value) * (kvp1.Value - 1) / 2) * map[thirdElementValue];
+                        }
+                        else if (kvp1.Key < kvp2.Key && kvp2.Key < thirdElementValue)
+                        {
+                            output += kvp1.Value * kvp2.Value * map[thirdElementValue];
+                        }
+                    }
+
+                    output = output % 1000000007;
+                }
+            }
+            return (int)output;
+        }
+    }
+}
