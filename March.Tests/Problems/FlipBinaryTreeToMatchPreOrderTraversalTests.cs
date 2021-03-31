92ab3ad5e280a98365f475a95b2263107cf12c88
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-03-31 15:53:33 +0800

    Day 29: Flip Binary Tree To Match PreOrder Traversal

---
+++ March.Tests/Problems/FlipBinaryTreeToMatchPreOrderTraversalTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Shared.Models;
+using System.Collections.Generic;
+using March.Problems;
+
+namespace March.Tests.Problems
+{
+    public class FlipBinaryTreeToMatchPreOrderTraversalTests
+    {
+        [Test, TestCaseSource(nameof(TestCaseSource))]
+        public void FlipMatchVoyage(TreeNode root, int[] voyage, IList<int> expected)
+        {
+            var sut = new FlipBinaryTreeToMatchPreOrderTraversal();
+            var actual = sut.FlipMatchVoyage(root, voyage);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static object[] TestCaseSource => new object[]
+        {
+            new object[]
+            {
+                new TreeNode(1, new TreeNode(2)), 
+                new[] {2, 1},
+                new List<int> { -1 }, 
+            },
+            new object[]
+            {
+                new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+                new[] {1, 2, 3},
+                new List<int>(),
+            },
+            new object[]
+            {
+                new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+                new[] {1, 3, 2},
+                new List<int> { 1 },
+            }
+        };
+    }
+}
---
+++ March/Problems/FlipBinaryTreeToMatchPreOrderTraversal.cs
@@ -0,0 +1,51 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System.Collections.Generic;
+
+namespace March.Problems
+{
+    public class FlipBinaryTreeToMatchPreOrderTraversal : IProblem
+    {
+        public string Title => "Flip Binary Tree To Match PreOrder Traversal";
+        public string Url => "https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/592/week-5-march-29th-march-31st/3689/";
+
+        private int[] _voyage;
+        private int _index;
+        private List<int> _output;
+        public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+        {
+            _voyage = voyage;
+            _index = 0;
+            _output = new List<int>();
+            Dfs(root);
+            return _output;
+        }
+
+        private void Dfs(TreeNode root)
+        {
+            if (root == null) return;
+
+            if (root.Value != _voyage[_index++])
+            {
+                _output.Clear();
+                _output.Add(-1);
+                return;
+            }
+
+            if (root.Left != null && root.Left.Value != _voyage[_index])
+            {
+                if (_output.Count == 0 || _output[0] != -1)
+                {
+                    _output.Add(root.Value);
+                    Dfs(root.Right);
+                    Dfs(root.Left);
+                }
+            }
+            else
+            {
+                Dfs(root.Left);
+                Dfs(root.Right);
+            }
+        }
+    }
+}
