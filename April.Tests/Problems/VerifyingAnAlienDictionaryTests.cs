473fe7eb935c0d0052ae58e4f54203339deec1a4
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-04-10 00:11:51 +0800

    Day 9: Verifying an Alien Dictionary

---
+++ April.Tests/Problems/VerifyingAnAlienDictionaryTests.cs
@@ -0,0 +1,18 @@
+﻿using April.Problems;
+using NUnit.Framework;
+
+namespace April.Tests.Problems
+{
+    public class VerifyingAnAlienDictionaryTests
+    {
+        [TestCase(new[] { "hello", "leetcode" }, "hlabcdefgijkmnopqrstuvwxyz", true)]
+        [TestCase(new[] { "word", "world", "row" }, "worldabcefghijkmnpqstuvxyz", false)]
+        [TestCase(new[] { "apple", "app" }, "abcdefghijklmnopqrstuvwxyz", false)]
+        public void IsAlienSorted(string[] words, string order, bool expected)
+        {
+            var sut = new VerifyingAnAlienDictionary();
+            var actual = sut.IsAlienSorted(words, order);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ April/Problems/VerifyingAnAlienDictionary.cs
@@ -0,0 +1,32 @@
+﻿using Shared.Interfaces;
+
+namespace April.Problems
+{
+    public class VerifyingAnAlienDictionary : IProblem
+    {
+        public string Title => "Verifying an Alien Dictionary";
+        public string Url => "https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3702/";
+
+        public bool IsAlienSorted(string[] words, string order)
+        {
+            var map = new int[26];
+            for (var i = 0; i < order.Length; i++)
+            {
+                map[order[i] - 'a'] = i;
+            }
+
+            for (var i = 0; i < words.Length - 1; i++)
+            {
+                for (var j = 0; j < words[i].Length; j++)
+                {
+                    if (j >= words[i + 1].Length) return false;
+
+                    if (map[words[i][j] - 'a'] < map[words[i + 1][j] - 'a']) break;
+
+                    if (map[words[i][j] - 'a'] > map[words[i + 1][j] - 'a']) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
