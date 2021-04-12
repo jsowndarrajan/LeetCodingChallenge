ba52dac9f51b5dc561813f7f688bf7de1b6dcc17
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-04-12 20:49:02 +0800

    Day 12: Beautiful Arrangement II

---
+++ April.Tests/Problems/BeautifulArrangement2Tests.cs
@@ -0,0 +1,20 @@
+﻿using April.Problems;
+using NUnit.Framework;
+
+namespace April.Tests.Problems
+{
+    public class BeautifulArrangement2Tests
+    {
+        [TestCase(3, 1, new[] { 1, 2, 3 })]
+        [TestCase(3, 2, new[] { 1, 3, 2 })]
+        [TestCase(4, 2, new[] { 1, 3, 2, 4 })]
+        [TestCase(4, 3, new[] { 1, 4, 2, 3 })]
+        [TestCase(5, 2, new[] { 1, 3, 2, 4, 5 })]
+        public void ConstructArray(int n, int k, int[] expected)
+        {
+            var sut = new BeautifulArrangement2();
+            var actual = sut.ConstructArray(n, k);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ April/Problems/BeautifulArrangement2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Interfaces;
+
+namespace April.Problems
+{
+    public class BeautifulArrangement2 : IProblem
+    {
+        public string Title => "Beautiful Arrangement II";
+        public string Url => "https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3705/";
+
+        public int[] ConstructArray(int n, int k)
+        {
+            var ans = new int[n];
+            var low = 1;
+            var high = k + 1;
+
+            for (var i = 0; i <= k; i++)
+            {
+                if (i % 2 == 0) ans[i] = low++;
+                else ans[i] = high--;
+            }
+
+            for (var i = k + 1; i < n; i++)
+            {
+                ans[i] = i + 1;
+            }
+
+            return ans;
+        }
+    }
+}
