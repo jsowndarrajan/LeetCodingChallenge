65a945a8b9d7dc4f4941053a6c414e9209e4177a
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-04-15 01:21:34 +0800

    Day 14: Partition List

---
+++ April.Tests/Problems/PartitionListTests.cs
@@ -0,0 +1,28 @@
+﻿using April.Problems;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Shared.Models;
+
+namespace April.Tests.Problems
+{
+    public class PartitionListTests
+    {
+        [Test, TestCaseSource(nameof(TestCaseSource))]
+        public void Partition(ListNode head, int x, ListNode expected)
+        {
+            var sut = new PartitionList();
+            var actual = sut.Partition(head, x);
+            Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+
+        private static object[] TestCaseSource => new object[]
+        {
+            new object[]
+            {
+                new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(2)))))),
+                3,
+                new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(4, new ListNode(3, new ListNode(5)))))),
+            }
+        };
+    }
+}
---
+++ April/Problems/PartitionList.cs
@@ -0,0 +1,40 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+
+namespace April.Problems
+{
+    public class PartitionList : IProblem
+    {
+        public string Title => "Partition List";
+        public string Url => "https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3707/";
+
+        public ListNode Partition(ListNode head, int x)
+        {
+            ListNode firstHalfStart = new ListNode(-1);
+            ListNode firstHalfEnd = firstHalfStart;
+
+            ListNode secondHalfStart = new ListNode(-1);
+            ListNode secondHalfEnd = secondHalfStart;
+
+            while (head != null)
+            {
+                if (head.Value < x)
+                {
+                    firstHalfEnd.Next = head;
+                    firstHalfEnd = firstHalfEnd.Next;
+                }
+                else
+                {
+                    secondHalfEnd.Next = head;
+                    secondHalfEnd = secondHalfEnd.Next;
+                }
+                head = head.Next;
+            }
+
+            firstHalfEnd.Next = secondHalfStart.Next;
+            secondHalfEnd.Next = null;
+
+            return firstHalfStart.Next;
+        }
+    }
+}
