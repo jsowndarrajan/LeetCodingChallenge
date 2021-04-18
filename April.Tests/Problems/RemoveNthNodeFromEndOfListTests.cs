014642f5bad0925640d0a92e37b5ddbd77f58dbb
Sowndarrajan Jayapal <sowndarrajan.j@gmail.com>
2021-04-18 23:20:19 +0800

    Day 18: Remove Nth Node From End of List

---
+++ April.Tests/Problems/RemoveNthNodeFromEndOfListTests.cs
@@ -0,0 +1,46 @@
+﻿using April.Problems;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Shared.Models;
+
+namespace April.Tests.Problems
+{
+    public class RemoveNthNodeFromEndOfListTests
+    {
+        [Test, TestCaseSource(nameof(TestCaseSource))]
+        public void RemoveNthFromEnd(ListNode head, int n, ListNode expected)
+        {
+            var sut = new RemoveNthNodeFromEndOfList();
+            var actual = sut.RemoveNthFromEnd(head, n);
+            Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+
+        private static object[] TestCaseSource => new object[]
+        {
+            new object[]
+            {
+                new ListNode(1),
+                1,
+                null
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2)),
+                2,
+                new ListNode(2)
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2)),
+                1,
+                new ListNode(1)
+            },
+            new object[]
+            {
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                2,
+                new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(5)))),
+            }
+        };
+    }
+}
---
+++ April/Problems/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,33 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+
+namespace April.Problems
+{
+    public class RemoveNthNodeFromEndOfList : IProblem
+    {
+        public string Title => "Remove Nth Node From End of List";
+        public string Url => "https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/595/week-3-april-15th-april-21st/3712/";
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var output = new ListNode(-1, head);
+            var thresholdPoint = output;
+            while (n > 0)
+            {
+                thresholdPoint = thresholdPoint.Next;
+                n--;
+            }
+
+            var current = output;
+            while (thresholdPoint != null && thresholdPoint.Next != null)
+            {
+                current = current.Next;
+                thresholdPoint = thresholdPoint.Next;
+            }
+
+            current.Next = current.Next.Next;
+
+            return output.Next;
+        }
+    }
+}
